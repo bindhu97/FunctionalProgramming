dd815f72f7b0b23d0365b048f6926d0cb5796b58
bindhu97 <binduvishwakarma97@gmail.com>
2022-11-30 16:06:19 +0530

    Leap Year

---
+++ LeapYear.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FunctionalProgramming
+{
+    class LeapYear
+    {
+        public void leap()
+        {
+                Console.WriteLine("Hello World!");
+        }
+    }
+}
